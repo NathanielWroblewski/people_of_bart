fe8fd7c9ff3777831539ac68c99fb49ca1acf16a
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2014-02-28 18:22:40 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeopleOfBart::Application.config.secret_key_base = '2f4ff940cc15d1458997fcff072bb6e3e61bb8ecda3b45a1bd3cc1722439632869d2eecb69a9fb0e71baa49f20f67ee04ae4e6ff28b71e174892a4a0f40baa95'
